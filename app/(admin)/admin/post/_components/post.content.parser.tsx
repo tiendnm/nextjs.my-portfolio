9353b1a403177094edd04dcf57907c1229ef0e34
Đỗ Ngọc Minh Tiến <tien.dnm@outlook.com>
2023-05-29 01:28:15 +0700

    add copy to clipboard button for code component

--- app/(admin)/admin/post/_components/post.content.parser.tsx
+++ app/(admin)/admin/post/_components/post.content.parser.tsx
@@ -1,34 +1,37 @@
 "use client";
 
-import parse, { Element, Text, domToReact, DOMNode } from "html-react-parser";
-import Image from "next/image";
-import clsx from "clsx";
+import { mdiContentDuplicate } from "@mdi/js";
+import Icon from "@mdi/react";
 import { parseStyleString } from "@utils/datatype";
-import { useCallback, useMemo } from "react";
 import { BLUR_URL } from "@variables";
+import useNotification from "antd/es/notification/useNotification";
+import clsx from "clsx";
+import parse, { DOMNode, Element, Text, domToReact } from "html-react-parser";
+import Image from "next/image";
+import { useCallback, useMemo } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import * as theme from "react-syntax-highlighter/dist/esm/styles/hljs";
 export default function PostContentParser({ content }: { content?: string }) {
-  const recursiveFindImage = useCallback((element: Element): Element | undefined => {
-    const children = element?.children as Element[];
-    if (children?.length > 0) {
-      const findImage = children.find((x) => x.name === "img");
-      if (findImage) {
-        return findImage;
-      } else {
-        for (let index = 0; index < children.length; index++) {
-          const childElement = children[index];
-          const check = recursiveFindImage(childElement);
-          if (check) {
-            return check;
-          }
-        }
-        return undefined;
-      }
-    } else {
-      return undefined;
-    }
-  }, []);
+  const [notiApi, contextHolder] = useNotification();
+
+  const copyCode = useCallback(
+    (text: string) => {
+      navigator.clipboard.writeText(text);
+      notiApi.success({
+        message: "Thông báo",
+        description: (
+          <>
+            <div>Đã sao chép vào bảng tạm:</div>
+            <pre className="h-6 overflow-hidden text-ellipsis bg-gray-700 text-white">
+              {text}
+            </pre>
+          </>
+        ),
+        placement: "bottom",
+      });
+    },
+    [notiApi]
+  );
 
   const options = useMemo(
     () => ({
@@ -61,18 +64,38 @@ export default function PostContentParser({ content }: { content?: string }) {
           return domToReact(children, options);
         }
         if (name === "code" && attribs.class?.startsWith("language")) {
+          const textChildren = children as Text[];
+          const text = textChildren?.[0].data;
           return (
-            <SyntaxHighlighter
-              language={attribs.class.replace("language-", "")}
-              style={theme.dracula}>
-              {domToReact(children, options) as any}
-            </SyntaxHighlighter>
+            <div className="relative w-full">
+              <span
+                onClick={() => {
+                  copyCode(text);
+                }}
+                className="absolute right-2 top-2 cursor-pointer bg-gray-800/90 p-1 text-white hover:text-gray-300 active:text-gray-500">
+                <Icon
+                  path={mdiContentDuplicate}
+                  className="  "
+                  size={0.8}
+                />
+              </span>
+              <SyntaxHighlighter
+                language={attribs.class.replace("language-", "")}
+                style={theme.dracula}>
+                {text}
+              </SyntaxHighlighter>
+            </div>
           );
         }
       },
     }),
-    []
+    [copyCode]
   );
 
-  return <div className="ck-content">{parse(`${content}`, options)}</div>;
+  return (
+    <>
+      {contextHolder}
+      <div className="ck-content">{parse(`${content}`, options)}</div>
+    </>
+  );
 }
